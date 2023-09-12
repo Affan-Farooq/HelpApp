2e779676c65b705fc50410670ce98702e9343401
Affan <22maf@queensu.ca>
2023-09-12 15:39:49 -0400

    Added the multiply and divide functions, as well as the 'main' trigger

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ def add(num1, num2):
 def subtract(num1, num2):
     return num1 - num2
 
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1/num2
+
 def main():
     print(add(3, 7))
     print(subtract(10, 7))
+    print(multiply(3, 5))
+    print(divide(40, 8))
+
+main()
